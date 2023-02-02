0cf932c3f70a945a3fbb277e8d20f2b62b2f84c3
Joe Lombrozo <joe@djeebus.net>
2023-02-02 09:58:35 -0600

    some clean up

--- main.py
+++ main.py
@@ -49,27 +49,8 @@ def move(game_state: typing.Dict) -> typing.Dict:
 
     is_move_safe = {"up": True, "down": True, "left": True, "right": True}
 
-    # We've included code to prevent your Battlesnake from moving backwards
-    my_head = game_state["you"]["body"][0]  # Coordinates of your head
-    my_neck = game_state["you"]["body"][1]  # Coordinates of your "neck"
-
     check_body_position(is_move_safe, game_state)
-
-    # TODO: Step 1 - Prevent your Battlesnake from moving out of bounds
-    board_width = game_state['board']['width']
-    board_height = game_state['board']['height']
-    if is_move_safe["up"] and my_head["y"] == board_height - 1:
-        is_move_safe["up"] = False
-    if is_move_safe["down"] and my_head["y"] == 0:
-        is_move_safe["down"] = False
-    if is_move_safe["left"] and my_head["x"] == 0:
-        is_move_safe["left"] = False
-    if is_move_safe["right"] and my_head["x"] == board_width - 1:
-        is_move_safe["right"] = False
-
-    # TODO: Step 2 - Prevent your Battlesnake from colliding with itself
-    my_body = game_state['you']['body'][1:]
-
+    check_board_boundaries(is_move_safe, game_state)
 
     # TODO: Step 3 - Prevent your Battlesnake from colliding with other Battlesnakes
     # opponents = game_state['board']['snakes']
@@ -92,6 +73,21 @@ def move(game_state: typing.Dict) -> typing.Dict:
 
     print(f"MOVE {game_state['turn']}: {next_move}")
     return {"move": next_move}
+
+
+def check_board_boundaries(is_move_safe, game_state):
+    my_head = game_state["you"]["body"][0]  # Coordinates of your head
+
+    board_width = game_state['board']['width']
+    board_height = game_state['board']['height']
+    if is_move_safe["up"] and my_head["y"] == board_height - 1:
+        is_move_safe["up"] = False
+    if is_move_safe["down"] and my_head["y"] == 0:
+        is_move_safe["down"] = False
+    if is_move_safe["left"] and my_head["x"] == 0:
+        is_move_safe["left"] = False
+    if is_move_safe["right"] and my_head["x"] == board_width - 1:
+        is_move_safe["right"] = False
 
 
 def move_left(d: dict):
